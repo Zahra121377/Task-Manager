fd59f374cbc4174a006f392096701d0269429d61
z.afkhami <afkhami121377@gmail.com>
2023-11-27 01:33:01 +0330

    Register Task model and customize admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
+from django.contrib.auth.models import Group, User
 
 # Register your models here.
+admin.site.register(Task)
+admin.site.unregister(Group)
+admin.site.unregister(User)
