aecbd4c856a6d73d20e1ddc45090ef4679f26920
Zahra121377 <afkhami121377@gmail.com>
2023-11-28 17:38:40 +0330

    Update admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,20 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-
+from django.contrib import messages
 from .models import Task
 
 # Register your models here.
 admin.site.unregister(Group)
 admin.site.unregister(User)
 
-
-@admin.action(description="Mark selected Tasks as completed")
-def make_completed(modeladmin, request, queryset):
-    queryset.update(completed=True)
-
-
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ("title", "completed", "remaining_time", "priority")
     list_filter = ["completed"]
-    actions = [make_completed]
+    actions = ["make_completed", "make_uncompleted"]
+    
+
+    @admin.action(description="Mark selected Tasks as completed")
+    def make_completed(self, request, queryset):
+        updated = queryset.update(completed=True)
+        message = "Tasks are marked completed successfuly."
+        self.message_user(request, message, messages.SUCCESS) 
+        
+    @admin.action(description="Mark selected Tasks as uncompleted")
+    def make_uncompleted(self, request, queryset):
+        updated = queryset.update(completed=False)
+        message = "Tasks are marked uncompleted successfuly."
+        self.message_user(request, message, messages.SUCCESS)
